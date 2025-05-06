9a7db33215f71daa4b7e0b5074224db5df4f362b
inf3niteNoob <abash.andria@gmail.com>
2025-05-06 22:46:39 +0400

    dark/light mode redesign
    added: new dark/light mode toggle and new email

--- dark-light-mode-toggle.js
+++ dark-light-mode-toggle.js
@@ -2,12 +2,15 @@ var isDark = false
 
 const toggleDarkMode = () => {
     isDark = !isDark;
+    const darkModeButton = document.querySelector('.button[onclick="toggleDarkMode()"]');
     if(isDark){//add dark to body class
         document.body.classList.remove("light");
         document.body.classList.add("dark");
+        darkModeButton.textContent = "🌞 Light Mode";
     } else {//remove dark from body class
         document.body.classList.remove("dark");
         document.body.classList.add("light");
+        darkModeButton.textContent = "🌙 Dark Mode";
     }
 
 }
